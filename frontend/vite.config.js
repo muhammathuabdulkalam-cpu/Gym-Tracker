6999245d210b1ab81ebd7e503bef4c5cb76c2cfa
Muhammathu Abdulkalam <muhammathuabdulkalam@VENR-06.local>
2026-04-09 17:53:09 +0530

    updated

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -1,7 +1,34 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
+import { VitePWA } from 'vite-plugin-pwa'
 
-// https://vite.dev/config/
 export default defineConfig({
-  plugins: [react()],
+  plugins: [
+    react(),
+    VitePWA({
+      registerType: 'autoUpdate',
+
+      manifest: {
+        name: 'Tranzio Gym',
+        short_name: 'Tranzio',
+        description: 'Gym Tracking App',
+        theme_color: '#ff4e5b',
+        background_color: '#000000',
+        display: 'standalone',
+        start_url: '/',
+        icons: [
+          {
+            src: '/tranzio_logo.png',
+            sizes: '192x192',
+            type: 'image/png'
+          },
+          {
+            src: '/tranzio_logo.png',
+            sizes: '512x512',
+            type: 'image/png'
+          }
+        ]
+      }
+    })
+  ]
 })
